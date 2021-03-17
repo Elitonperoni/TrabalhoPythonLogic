d6d1936cd930393b73b2e9a138cb4933da305c5d
Eliton Peroni <eliton_peroni@hotmail.com>
2021-03-17 19:58:42 -0300

    Trabalho 1

--- EX6.py
+++ EX6.py
@@ -3,7 +3,6 @@
 #Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
 #    Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg
 #    Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total. Escreva um algoritmo para ler a quantidade (em Kg) de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
-
 qtdMacaAdquirida = float(input("Digite a quantia(Kg) de maçã adquirida: "))
 qtdMorangoAdquirida = float(input("Digite  a quantia(Kg) de Morango adquirida: "))
 
@@ -16,7 +15,7 @@ if qtdMorangoAdquirida <= 5:
     precoMorango = 2.5
 elif qtdMorangoAdquirida > 5:
     precoMorango = 2.2
-
+##
 qtdTotalAdquirida = qtdMacaAdquirida + qtdMorangoAdquirida
 totalPagar = (qtdMacaAdquirida * precoMaca) + (qtdMorangoAdquirida * precoMorango)
 
